2ed0502f850724e7ce8f8a86beb9e4c639980e23
MinhDucNguyen <nguyenminhduc01032001@gmail.com>
2026-04-15 16:21:14 +0700

    up

--- src/app/api/settings.ts
+++ src/app/api/settings.ts
@@ -5,7 +5,7 @@ export async function fetchSiteSettings() {
         const data = await apiFetch("/site/settings", {
             next: { revalidate: 3600 }
         });
-        return data || null;
+        return data?.data || null;
     } catch (error) {
         console.error("fetchSiteSettings Error:", error);
         return null;
